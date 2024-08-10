534565599eed2bd636b52c42f4640a88e4001987
s862008 <s862008@yandex.ru>
2024-08-10 15:20:29 +0300

    доработки исправеления

--- src/app/main/components/smart-parametrs/smart-parametrs.component.ts
+++ src/app/main/components/smart-parametrs/smart-parametrs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {MatDialogRef} from "@angular/material/dialog";
 
@@ -7,7 +7,7 @@ import {MatDialogRef} from "@angular/material/dialog";
   templateUrl: './smart-parametrs.component.html',
   styleUrl: './smart-parametrs.component.css'
 })
-export class SmartParametrsComponent {
+export class SmartParametrsComponent implements OnInit {
   smartForm: FormGroup;
 
   constructor(private fb: FormBuilder, private dialogRef: MatDialogRef<any>) {
@@ -17,6 +17,58 @@ export class SmartParametrsComponent {
     });
   }
 
+  listChecked() {
+    const checkedInputs = document.querySelectorAll('input[type="checkbox"]:checked');
+    let choosed = "";
+    console.log("listChecked")
+    checkedInputs.forEach(input => {
+      const lblElement = input.parentNode?.querySelector('.fp-label--tx');
+      const parentElement = input.parentNode?.parentNode?.querySelector('h4');
+
+      const lbl = lblElement?.textContent;
+      const parent = parentElement?.innerText;
+      const id = input.id;
+
+      if (lbl) {
+        choosed += `<div class='fp-choosed__item'><span>${parent} ${lbl.toLowerCase()} </span><label for='${id}'> X </label></div>`;
+      }
+    });
+
+    const choosedBlock = document.getElementById("choosed-block");
+    if (choosedBlock) {
+      choosedBlock.innerHTML = choosed;
+    }
+  }
+
+
+  ngOnInit() {
+
+    this.listChecked();
+
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+      checkbox.addEventListener('change', (event) => {
+        if(checkbox.getAttribute('checked')==null){
+          checkbox.setAttribute('checked','checked')
+        }else{
+          checkbox.removeAttribute('checked');
+        }
+        this.listChecked();
+      });
+    });
+
+
+  }
+
+  clearFilter() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    console.log(checkboxes)
+    checkboxes.forEach(checkbox => {
+      checkbox.removeAttribute('checked');
+    });
+    console.log("clear")
+    this.listChecked();
+  }
 
   onSubmit() {
     if (this.smartForm.valid) {
@@ -28,4 +80,6 @@ export class SmartParametrsComponent {
   onCancel() {
     this.dialogRef.close();
   }
+
+
 }
--- src/app/main/pages/home/home.component.ts
+++ src/app/main/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {BannerComponent} from "../../components/banner/banner.component";
+
 import {MatDialog} from "@angular/material/dialog";
 import {SmartParametrsComponent} from "../../components/smart-parametrs/smart-parametrs.component";
 import {absoluteFrom} from "@angular/compiler-cli";
@@ -10,31 +10,34 @@ import {absoluteFrom} from "@angular/compiler-cli";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit  {
+export class HomeComponent implements OnInit {
 
-  category: string="Категория";
-  type: string ="ТИП";
+  category: string = "Категория";
+  type: string = "ТИП";
   cost: number = 10500;
-  count: number =  34;
-  tempText:string = "14";
-  dt: string ="10.10.24";
-  gk_cards: string[]= ['1','2'];
+  count: number = 34;
+  tempText: string = "14";
+  dt: string = "10.10.24";
+  gk_cards: string[] = ['1', '2'];
 
-  constructor(private dialog: MatDialog) {}
-  ngOnInit(): void {}
+  constructor(private dialog: MatDialog) {
+  }
+
+  ngOnInit(): void {
+  }
 
   openDialog() {
-    const dialogRef = this.dialog.open(SmartParametrsComponent,{height:'100%',width:'90%'});
+
+
+    const dialogRef = this.dialog.open(SmartParametrsComponent, {height: '100%', width: '95%'});
 
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
         console.log('Данные из формы:', result);
       }
+
     });
   }
-
-
-
 
 
 }
